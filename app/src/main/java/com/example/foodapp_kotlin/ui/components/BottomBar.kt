de9445990e03fa0a0eea13ee300a3a23ad23cc30
Imene Bentifraouine <bentifraouineimene@gmail.com>
2026-03-25 10:01:29 +0100

    space

--- app/src/main/java/com/example/foodapp_kotlin/ui/components/BottomBar.kt
+++ app/src/main/java/com/example/foodapp_kotlin/ui/components/BottomBar.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+
 import androidx.compose.ui.unit.sp
 
 @Composable
